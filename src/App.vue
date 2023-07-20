3d7a71c4f1e94aeeda5a43cdf62dd86ba1fa32ad
Ray Foss <ray@vblip.com>
2023-07-20 17:55:09 -0500

    suspense based solution.
    We're not here to talk about my transition.

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,38 @@
     <router-link :to="{ name: 'Home' }">Snazzy Fake Blog</router-link>
   </nav>
   <div class="container">
-    <router-view></router-view>
-    <div v-if="isLoading" >Loading...</div>
+    <router-view v-slot="{ Component, route }">
+      <template v-if="Component"><!-- Lazy loading support -->
+      <Transition mode="out-in" :key="route.path">
+      <KeepAlive>
+      <Suspense>
+        <template #default>
+          <component :is="Component" :key="route.path"/>
+        </template>
+        <template #fallback><div>Loading...</div></template>
+      </Suspense>
+      </KeepAlive>
+      </Transition>
+      </template>
+    </router-view>
   </div>
 </template>
 
 <script setup lang="ts">
-import usePageRequests from './composables/usePageRequests'
-const {isLoading} = usePageRequests()
 </script>
 <style lang="css">
 .container {
   max-width: 960px;
   margin: 0 auto;
+}
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
